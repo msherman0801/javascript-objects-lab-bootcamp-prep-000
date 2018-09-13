9cd6a73761949166796209f93eb234c0347b0188
Matthew Sherman <msherman0801@gmail.com>
2018-09-13 23:59:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(object,{[key]: value})
+}
